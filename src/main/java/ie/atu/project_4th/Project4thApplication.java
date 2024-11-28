204d0e52e7898b2ab49a4946d1ef56f3bbc1277f
denis <G00403126@atu.ie>
2024-11-28 19:40:11 +0000

    Start

---
+++ src/main/java/ie/atu/project_4th/Project4thApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.project_4th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project4thApplication.class, args);
+    }
+
+}
